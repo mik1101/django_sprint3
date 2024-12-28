fde30e269bb9c6baec020c838cea80e187d10acc
Dev Mik <mikdev4sec@gmail.com>
2024-12-28 06:42:10 +0300

    Смена отступов с 2 на 4

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -5,57 +5,57 @@ from pages.models import Publichinfo
 
 class Location(Publichinfo):
 
-  name = models.CharField(max_length=256, verbose_name="Название места")
+    name = models.CharField(max_length=256, verbose_name="Название места")
 
-  class Meta:
-      verbose_name = "местоположение"
-      verbose_name_plural = "Местоположения"
+    class Meta:
+        verbose_name = "местоположение"
+        verbose_name_plural = "Местоположения"
 
 
 class Category(Publichinfo):
 
-  title = models.CharField(max_length=256, verbose_name="Заголовок")
+    title = models.CharField(max_length=256, verbose_name="Заголовок")
 
-  description = models.TextField(verbose_name="Описание")
+    description = models.TextField(verbose_name="Описание")
 
-  slug = models.SlugField(
-      unique=True,
-      verbose_name="Идентификатор",
-      help_text="Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.",
-      )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name="Идентификатор",
+        help_text="Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.",
+    )
 
-  class Meta:
-      verbose_name = "категория"
-      verbose_name_plural = "Категории"
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "Категории"
 
 
 class Post(Publichinfo):
 
-  title = models.CharField(max_length=256, verbose_name="Заголовок")
+    title = models.CharField(max_length=256, verbose_name="Заголовок")
 
-  text = models.TextField(verbose_name="Текст")
+    text = models.TextField(verbose_name="Текст")
 
-  pub_date = models.DateTimeField(
-      verbose_name="Дата и время публикации",
-      help_text="Если установить дату и время в будущем — можно делать отложенные публикации.",
-  )
+    pub_date = models.DateTimeField(
+        verbose_name="Дата и время публикации",
+        help_text="Если установить дату и время в будущем — можно делать отложенные публикации.",
+    )
 
-  author = models.ForeignKey(
-      get_user_model(), on_delete=models.CASCADE, verbose_name="Автор публикации"
-  )
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, verbose_name="Автор публикации"
+    )
 
-  location = models.ForeignKey(
-      Location,
-      on_delete=models.SET_NULL,
-      null=True,
-      blank=True,
-      verbose_name="Местоположение",
-  )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Местоположение",
+    )
 
-  category = models.ForeignKey(
-      Category, on_delete=models.SET_NULL, null=True, verbose_name="Категория"
-  )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, verbose_name="Категория"
+    )
 
-  class Meta:
-      verbose_name = "публикация"
-      verbose_name_plural = "Публикации"
+    class Meta:
+        verbose_name = "публикация"
+        verbose_name_plural = "Публикации"
--- blogicum/pages/models.py
+++ blogicum/pages/models.py
@@ -3,13 +3,15 @@ from django.db import models
 
 class Publichinfo(models.Model):
 
-  is_published = models.BooleanField(
-    default=True,
-    verbose_name="Опубликовано",
-    help_text="Снимите галочку, чтобы скрыть публикацию.",
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name="Опубликовано",
+        help_text="Снимите галочку, чтобы скрыть публикацию.",
     )
 
-  created_at = models.DateTimeField(auto_now_add=True, verbose_name="Добавлено")
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Добавлено"
+        )
 
-  class Meta:
-     abstract = True
+    class Meta:
+        abstract = True
