546afe2c1c219b6db6c2eef456c886b1df460aff
Dev Mik <mikdev4sec@gmail.com>
2024-12-28 06:33:35 +0300

    Поправка код согласно pip8

--- blogicum/pages/models.py
+++ blogicum/pages/models.py
@@ -5,15 +5,11 @@ class Publichinfo(models.Model):
 
   is_published = models.BooleanField(
     default=True,
-    verbose_name='Опубликовано',
-    help_text='Снимите галочку, чтобы скрыть публикацию.'
+    verbose_name="Опубликовано",
+    help_text="Снимите галочку, чтобы скрыть публикацию.",
     )
 
-  created_at = models.DateTimeField(
-    auto_now_add=True,
-    verbose_name='Добавлено'
-    )
-
+  created_at = models.DateTimeField(auto_now_add=True, verbose_name="Добавлено")
 
   class Meta:
-    abstract = True
+     abstract = True
